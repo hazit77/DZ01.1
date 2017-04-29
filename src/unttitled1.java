14bea345de344c6b7845e49c8ae689daa4cdca2e
hazit77 <andriyhazit@gmail.com>
2017-04-29 16:29:07 +0300

    Initial commit

---
+++ src/unttitled1.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+
+public class unttitled1 {
+    public static void main(String[] args) {
+        System.out.println("Enter Your name ");
+        Scanner in = new Scanner(System.in);
+        String c = in.nextLine();
+        System.out.println("Hello, my dear  " + c);
+
+    }
+}
